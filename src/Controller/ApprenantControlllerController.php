47ae703114cb431a23b91640971240b238423e39
Fcordier7921 <f.cordier@codeur.online>
2021-02-19 16:44:26 +0100

    rajout ddb2

--- src/Controller/ApprenantControlllerController.php
+++ src/Controller/ApprenantControlllerController.php
@@ -12,8 +12,7 @@ use Symfony\Component\HttpFoundation\File\Exception\FileException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\String\Slugger\SluggerInterface;
-use Symfony\Component\Mime\MimeTypes;
+
 
 
 /**
